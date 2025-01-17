b4b8182b68054987d47df146a5e14c0e078928f6
Mihajlo Levi <mihajlo.levi@proton.me>
2025-01-17 01:03:03 +0100

    initial commit

---
+++ app/news/[id]/not-found.js
@@ -0,0 +1,10 @@
+const NewsNotFoundPage = () => {
+  return (
+    <div id='error'>
+      <h1>Article not Found!</h1>
+      <p>We could not find the requested article</p>
+    </div>
+  );
+};
+
+export default NewsNotFoundPage;
